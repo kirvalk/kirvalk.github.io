8ae08e0c17d2d5739ae08a3784a42c8215a83a45
karagedov <k.karagedov@cft.ru>
2020-03-28 14:36:47 +0700

    s

--- sub-frame.js
+++ sub-frame.js
@@ -4,6 +4,18 @@ function storageAccessAPISupported() {
   return 'hasStorageAccess' in document && 'requestStorageAccess' in document;
 }
 
+function ready(fn) {
+  if (
+    document.attachEvent
+      ? document.readyState === 'complete'
+      : document.readyState !== 'loading'
+  ) {
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
 function accessStorage(fn) {
   document.hasStorageAccess().then(
     function(hasAccess) {
